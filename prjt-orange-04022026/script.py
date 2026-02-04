aaaf94235c050805f8db71be7f8b16371cb94bf1
HAMZAZAWAK17 <ezzouekhamza2411@gmail.com>
2026-02-04 10:58:19 +0000

    feat: Add initial Python script demonstrating basic variables, I/O, arithmetic, string concatenation, and comparison operations.

--- prjt-orange-04022026/script.py
+++ prjt-orange-04022026/script.py
@@ -64,3 +64,25 @@ chaine1 = input("Entrez la premiere chaine: ")
 chaine2 = input("Entrez la deuxieme chaine: ")
 print("La concatenation de", chaine1, "et", chaine2, "est", chaine1 + chaine2)
 
+
+#La comparaison avec un resultat qui est boolean:
+x = 10
+y = 20
+print("x =", x, "y =", y)
+
+# Egalité (==)
+print("Est-ce que x est egal a y ?", x == y)
+
+# Différence (!=)
+print("Est-ce que x est different de y ?", x != y)
+
+# Supérieur et Inférieur
+print("Est-ce que x est superieur a y ?", x > y)
+print("Est-ce que x est inferieur a y ?", x < y)
+
+# Supérieur ou égal / Inférieur ou égal
+print("Est-ce que x est superieur ou egal a 10 ?", x >= 10)
+print("Est-ce que x est inferieur ou egal a 15 ?", x <= 15)
+
+
+
