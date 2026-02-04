7bbb576bf0d3ce47e038de429706c8d60859b475
HAMZAZAWAK17 <ezzouekhamza2411@gmail.com>
2026-02-04 11:29:41 +0000

    feat: Add initial script demonstrating basic Python concepts including variables, input/output, arithmetic and logical operators, and built-in functions.

--- prjt-orange-04022026/script.py
+++ prjt-orange-04022026/script.py
@@ -91,3 +91,33 @@ print("or:", x > y or x < y)
 print("not:", not (x > y))
 
 
+
+#Les fontions inregrees:
+texte_exemple = "Python ODC"
+nombre_negatif = -4.8
+
+print("--- Fonctions integrees ---")
+print("Variable texte:", texte_exemple)
+print("Variable nombre:", nombre_negatif)
+
+# len(): Longueur d'une chaine
+print("Longueur de la chaine (len):", len(texte_exemple))
+
+# type(): Type d'une variable
+print("Type de la variable nombre (type):", type(nombre_negatif))
+
+# abs(): Valeur absolue
+print("Valeur absolue (abs):", abs(nombre_negatif))
+
+# round(): Arrondir un nombre
+print("Arrondi (round):", round(nombre_negatif))
+
+# max() et min(): Maximum et Minimum
+print("Maximum (max) entre 5, 12, 1:", max(5, 12, 1))
+print("Minimum (min) entre 5, 12, 1:", min(5, 12, 1))
+
+# int() / float() / str(): Conversion de type
+print("Conversion en entier (int):", int(nombre_negatif))
+print("Conversion en string (str):", str(123) + "4")
+
+
