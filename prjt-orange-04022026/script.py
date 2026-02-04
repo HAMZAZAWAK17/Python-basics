dfaa20429c5e16d9df0ea04d0b8762fa98b93635
HAMZAZAWAK17 <ezzouekhamza2411@gmail.com>
2026-02-04 11:52:44 +0000

    feat: Introduce `script.py` demonstrating Python basics including variables, I/O, arithmetic, comparisons, built-in functions, and conditional statements.

--- prjt-orange-04022026/script.py
+++ prjt-orange-04022026/script.py
@@ -140,3 +140,26 @@ elif operation == "/":
 else:
     print("invalid operation")
 
+
+
+
+#exemple 2 : determiner si un nombre est par ou impair:
+nombre = int(input("enter a number"))
+
+if nombre % 2 == 0:
+    print("the number is even")
+else:
+    print("the number is odd")
+
+
+
+
+#exemple 3 : determiner si un nombre est positif ou negatif:
+nombre = int(input("enter a number"))
+
+if nombre > 0:
+    print("the number is positive")
+elif nombre < 0:
+    print("the number is negative")
+else:
+    print("the number is zero")
