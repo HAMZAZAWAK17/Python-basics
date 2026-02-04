10dd4998f57a147223293a060b6e4199df305d18
HAMZAZAWAK17 <ezzouekhamza2411@gmail.com>
2026-02-04 12:20:52 +0000

    feat: Add `script.py` demonstrating basic Python concepts including variables, operations, input, built-in functions, and conditional statements.

--- prjt-orange-04022026/script.py
+++ prjt-orange-04022026/script.py
@@ -163,3 +163,19 @@ elif nombre < 0:
     print("the number is negative")
 else:
     print("the number is zero")
+
+
+#exmeple 4: evaluation de la note d'un etudiant: tres bien / bien / moyen / passable / insuffisant:
+note = int(input("enter the note"))
+
+if note >= 18:
+    print("tres bien")
+elif note >= 14:
+    print("bien")
+elif note >= 10:
+    print("moyen")
+elif note >= 6:
+    print("passable")
+else:
+    print("insuffisant")
+
