1dcba6af658271a91fcc5fb1c1e0ee7a22dd5875
HAMZAZAWAK17 <ezzouekhamza2411@gmail.com>
2026-02-05 11:00:17 +0000

    feat: Add Python script demonstrating various list operations on a fruit list.

--- prjt-orange-04022026/22_exercice_listes_fruits.py
+++ prjt-orange-04022026/22_exercice_listes_fruits.py
@@ -1,33 +1,25 @@
-# 1. Création de la liste
 fruits = ["pomme", "banane", "cerise", "orange", "kiwi"]
 
-# 2. Accéder aux éléments
-print(fruits[0])   # Premier
-print(fruits[-1])  # Dernier
-print(fruits[2])   # Troisième
+print(fruits[0])
+print(fruits[-1])
+print(fruits[2])
 
-# 3. Modifier des éléments
 fruits[1] = "mangue"
 print(fruits)
 
-# 4. Ajouter des éléments
-fruits.append("ananas")      # À la fin
-fruits.insert(0, "fraise")   # Au début
+fruits.append("ananas")
+fruits.insert(0, "fraise")
 print(fruits)
 
-# 5. Supprimer des éléments
 fruits.remove("kiwi")
-fruits.pop()                 # Enlève le dernier
+fruits.pop()
 print(fruits)
 
-# 6. Afficher une partie
-print(fruits[:3])  # Les 3 premiers
-print(fruits[-2:]) # Les 2 derniers
+print(fruits[:3])
+print(fruits[-2:])
 
-# 7. Longueur
 print(len(fruits))
 
-# 8. Recherche
 if "banane" in fruits:
     print("Banane est là")
 
@@ -36,6 +28,5 @@ if "peche" in fruits:
 else:
     print("Pêche n'est pas là")
 
-# 9. Boucle for
 for fruit in fruits:
     print(fruit)
